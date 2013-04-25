08e00bd9a1f092b2af6122f16bff52625723111b
Justin Campbell <justin@justincampbell.me>
2013-04-25 00:02:31 -0400

    Create HTTP endpoints

---
+++ src/url_shortener/handler.clj
@@ -0,0 +1,26 @@
+(ns url-shortener.handler
+  (:use compojure.core
+        [url-shortener.shortener :only (shorten expand)])
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defn redirect [url]
+  {:status 302 :headers {"Location" url}})
+
+(defroutes app-routes
+  (GET "/" []
+       (redirect "https://github.com/justincampbell/language-exploration"))
+
+  (GET "/shorten" [url]
+       (let [token (shorten url)
+             path (str "/" token)]
+         {:status 201 :body path}))
+
+  (GET "/:token" [token]
+       (let [full-url (expand token)]
+         (if full-url
+           (redirect full-url)
+           {:status 404}))))
+
+(def app
+  (handler/site app-routes))
---
+++ test/url_shortener/handler_test.clj
@@ -0,0 +1,31 @@
+(ns url-shortener.handler-test
+  (:use clojure.test
+        ring.mock.request
+        url-shortener.handler))
+
+(deftest test-app
+  (testing "/"
+    (let [response (app (request :get "/"))
+          status (:status response)
+          headers (:headers response)]
+      (is (= status 302))
+      (is (= (headers "Location")
+             "https://github.com/justincampbell/language-exploration"))))
+
+  (testing "/shorten"
+    (let [url "http://justincampbell.me"
+          response (app (request :get (str "/shorten?url=" url)))]
+      (is (= (:status response) 201))
+      (is (= (:body response) "/1"))))
+
+  (testing "/:token"
+    (let [url "http://justincampbell.me"
+          token "1"
+          path (str "/" token)
+          response (app (request :get path))]
+      (is (= (:status response) 302))
+      (is (= ((:headers response) "Location") url))
+
+    (testing "when not found"
+      (let [response (app (request :get "/noexistant"))]
+        (is (= (:status response) 404)))))))
