26206ef23657b64130076aa84302878606eac3a0
Justin Campbell <justin@justincampbell.me>
2013-04-24 23:58:22 -0400

    Add Compojure

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "https://github.com/justincampbell/url-shortener-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.3"]]
+  :ring {:handler url-shortener.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
